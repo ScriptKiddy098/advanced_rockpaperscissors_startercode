b5f8f4e54daf5413f4fa15e36a8c8617953a547d
Luis <luisrosero098@gmail.com>
2017-11-27 23:16:09 +0000

    Rough Draft

--- js/script.js
+++ js/script.js
@@ -28,3 +28,86 @@
 
 // DOCUMENT READY FUNCTION
 
+
+//OLD CODE----------------------------------------------------------------------
+//---- 3 images resembling choices, chocies contain values to resemble your choice, computers 
+
+        
+
+//var userChoice = prompt("Do you choose Rock, Paper or Scissors?");
+var computerChoice = Math.random();
+
+if (computerChoice <= 0.24) {
+    computerChoice = "Rock";
+} else if (computerChoice > 0.24 && computerChoice <= 0.50) {
+    computerChoice = "Paper";
+} else if (computerChoice > 0.50 && computerChoice <= 0.75) {
+    computerChoice = "Scissors";
+}
+
+
+console.log("Computer choose: " + computerChoice);
+
+var compare = function(choice1, choice2) {
+    if (choice1 === choice2) {
+        return userChoice = prompt("The result is a tie! Please make a new choice...");
+    }
+    
+    if (choice1 === "Rock") {
+        if (choice2 === "Paper") {
+            return "Paper Wins!";
+        } else if (choice2 === "Scissors") {
+            return "Rock Wins!";
+        } else {
+            return "Rope Wins!";
+        }
+    }
+
+    if (choice1 === "Paper") {
+        if (choice2 === "Rock") {
+            return "Rock Wins!";
+        } else if (choice2 === "Scissors") {
+            return "Scissors Wins!";
+        } else {
+            return "Rope Wins!";
+        }
+    }
+
+    if (choice1 === "Scissors") {
+        if (choice2 === "Rock") {
+            return "Rock Wins!";
+        } else if (choice2 === "Paper") {
+            return "Scissors Wins!";
+        } else {
+            return "Scissors Wins!";
+        }
+    }
+
+    if (choice1 === "Rope") {
+        if (choice2 === "Rock") {
+            return "Rope Wins!";
+        } else if (choice2 === "Paper") {
+            return "Rope Wins!";
+        } else {
+            return "Scissors Wins!";
+        }
+    }
+};
+
+
+$(document).ready(function()){ 
+        $('#Rock').click(function()){
+                compare('Rock',computerChoice);
+}; 
+        ///
+        $('#Scissors').click(function()){
+                compare('Scissors',computerChoice);
+}; 
+        ///
+        $('#Paper').click(function()){
+                compare('Paper',computerChoice);
+}; 
+)};
+//compare(userChoice,computerChoice);
+
+
